ec007966d212e6ebdd305e747b3c33564c48ae8a
mashimada2 <mshimada2@mail.csuchico.edu>
2019-10-04 12:55:07 -0700

    this commit fixes issue #6

---
+++ app/src/main/java/com/example/buddii/chooseRoute.java
@@ -0,0 +1,32 @@
+package com.example.buddii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class chooseRoute extends AppCompatActivity
+{
+    //private Button btnYes;
+    //private Button btnNo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_route);
+
+        //btnYes = findViewById(R.id.yesBuddii);
+
+        /*btnYes.setOnClickListener(new View.OnClickListener())
+        {
+            @Override
+                    public void onClick(View v)
+            {
+                moveTo
+            }
+
+        }*/
+    }
+}

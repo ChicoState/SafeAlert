fa145932d26f36e871fc2ea970c0cdb8ec68f425
DeWubs <rwprouty55@gmail.com>
2019-10-06 16:29:13 -0700

    fixed some buttons now only crashes on one of the buttons

--- app/src/main/java/com/example/buddii/beABuddy.java
+++ app/src/main/java/com/example/buddii/beABuddy.java
@@ -2,15 +2,13 @@ package com.example.buddii;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class chooseRoute extends AppCompatActivity {
+public class beABuddy extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_route);
+        setContentView(R.layout.activity_be_abuddy);
     }
 }

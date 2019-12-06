552b21f36dcaf51fb10f2bd371991ed8c8a3aabd
mshimada2 <mshimada2@mail.csuchico.edu>
2019-12-05 19:44:47 -0800

    working on login second attempt

--- app/src/main/java/com/example/buddii/ui/login/LoginViewModel.java
+++ app/src/main/java/com/example/buddii/ui/login/LoginViewModel.java
@@ -41,7 +41,8 @@ public class LoginViewModel extends ViewModel {
         }
     }
 
-    public void loginDataChanged(String username, String password) {
+    public void loginDataChanged(String username, String password)
+    {
         if (!isUserNameValid(username)) {
             loginFormState.setValue(new LoginFormState(R.string.invalid_username, null));
         } else if (!isPasswordValid(password)) {
@@ -52,11 +53,14 @@ public class LoginViewModel extends ViewModel {
     }
 
     // A placeholder username validation check
-    private boolean isUserNameValid(String username) {
-        if (username == null) {
+    private boolean isUserNameValid(String username)
+    {
+        if (username == null)
+        {
             return false;
         }
-        if (username.contains("@")) {
+        if (username.contains("@"))
+        {
             return Patterns.EMAIL_ADDRESS.matcher(username).matches();
         } else {
             return !username.trim().isEmpty();
@@ -64,7 +68,8 @@ public class LoginViewModel extends ViewModel {
     }
 
     // A placeholder password validation check
-    private boolean isPasswordValid(String password) {
+    private boolean isPasswordValid(String password)
+    {
         return password != null && password.trim().length() > 5;
     }
 }

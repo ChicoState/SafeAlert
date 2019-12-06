1786283a320425b0a8b933bd354dbfdd99f6d638
mshimada2 <mshimada2@mail.csuchico.edu>
2019-12-05 19:48:27 -0800

    working on login second attempt

--- app/src/main/java/com/example/buddii/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/buddii/ui/login/LoginActivity.java
@@ -20,13 +20,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.buddii.DBActivity;
+import com.example.buddii.DatabaseHandler;
 import com.example.buddii.MainActivity;
 import com.example.buddii.R;
-import com.example.buddii.chooseRoute;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
+        final Button registerButton = findViewById(R.id.registerButton);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
@@ -113,8 +116,15 @@ public class LoginActivity extends AppCompatActivity {
                 loadingProgressBar.setVisibility(View.VISIBLE);
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
+                Intent intent = new Intent (LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (LoginActivity.this, DBActivity.class);
                 startActivity(intent);
             }
         });

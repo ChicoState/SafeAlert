131800a1dff03f6d1f6bfc924d79212d1b1da641
Isaac <47043498+igs-code@users.noreply.github.com>
2019-12-17 13:14:48 -0800

    selecting buds updates

--- app/src/main/java/com/example/buddii/select_friends.java
+++ app/src/main/java/com/example/buddii/select_friends.java
@@ -2,28 +2,26 @@ package com.example.buddii;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class select_bud extends Activity
+public class select_friends extends Activity
 {
-    Button buddiiist, friendList;
-    private ArrayList<String> mbuddiiNames = new ArrayList<>();
-    private ArrayList<String> mbuddiiImages = new ArrayList<>();
+    private ArrayList<String> fbuddiiNames = new ArrayList<>();
+    private ArrayList<String> fbuddiiImages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.find_buds_list);
+        setContentView(R.layout.find_friends_list);
         addUsers();
     }
 
-//where the database info needs to be implemented; when this function calls initRecyclerView it is going to take the picture of
+    //where the database info needs to be implemented; when this function calls initRecyclerView it is going to take the picture of
     //the buddii and the name of the buddii then store it within the card view
     private void addUsers(){
 
@@ -35,26 +33,25 @@ public class select_bud extends Activity
             return;
         }
         String ArrayOfBuddies[];
-        ArrayOfBuddies=(dbHandler.loadUsers("name"));
+        ArrayOfBuddies=(dbHandler.loadFriends("name"));
         // mbuddiiNames.add("TESTING");
 
         // for loop will irretiate the index of ArrayOfBuddies and will
         // populate them into mBuddiNames ArrayList
         for (int i = 0 ; i < numOfBuddies; i++) {
 
-            mbuddiiNames.add(ArrayOfBuddies[i]);
+            fbuddiiNames.add(ArrayOfBuddies[i]);
         }
 
         initRecyclerView();
 
     }
     private void initRecyclerView(){
-        RecyclerView recyclerView = findViewById(R.id.findBudsList);
-        buddiisAdapter adapter = new buddiisAdapter(this, mbuddiiImages, mbuddiiNames);
+        RecyclerView recyclerView = findViewById(R.id.findFriendsList);
+        buddiisAdapter adapter = new buddiisAdapter(this, fbuddiiImages, fbuddiiNames);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
-
 
 

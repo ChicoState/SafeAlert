851efdd0d6634fe7e21e0905eefd3605532a9403
Isaac <47043498+igs-code@users.noreply.github.com>
2019-12-17 13:20:08 -0800

    usr profile revamp for mvc modeling testing

---
+++ app/src/main/java/com/example/buddii/ui/usrprofile/UsrProfileFragment.java
@@ -0,0 +1,39 @@
+package com.example.buddii.ui.usrprofile;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.buddii.R;
+
+public class UsrProfileFragment extends Fragment {
+
+    private UsrProfileViewModel mViewModel;
+
+    public static UsrProfileFragment newInstance() {
+        return new UsrProfileFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.usr_profile_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(UsrProfileViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/buddii/ui/usrprofile/UsrProfileViewModel.java
@@ -0,0 +1,7 @@
+package com.example.buddii.ui.usrprofile;
+
+import androidx.lifecycle.ViewModel;
+
+public class UsrProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

6e19e5547c1e151b53ac42325c33df5d250d1bfb
Isaac <47043498+igs-code@users.noreply.github.com>
2019-12-17 13:15:10 -0800

    Tutorial Activity updates

--- app/src/main/java/com/example/buddii/TutorialActivity.java
+++ app/src/main/java/com/example/buddii/TutorialActivity.java
@@ -1,11 +1,11 @@
 package com.example.buddii;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class TutorialActivity extends AppCompatActivity {
     private Button btnabt;
@@ -38,7 +38,7 @@ public class TutorialActivity extends AppCompatActivity {
     }
 
     private void moveToAbout(){
-        Intent intent = new Intent (TutorialActivity.this, AboutUsActivity.class);
+        Intent intent = new Intent (TutorialActivity.this, aboutUsActivity.class);
         startActivity(intent);
     }
 
